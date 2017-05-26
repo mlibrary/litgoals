0076c175f0a8d1c861f8f439b72bfd88847937d6
Bill Dueber <bill@dueber.com>
2017-05-26 17:18:04 -0400

    Formatting

--- litgoals.rb
+++ litgoals.rb
@@ -19,16 +19,15 @@ Sequel::Model.plugin :json_serializer
 
 # Set up logging and defaults for testing
 
-LOG = Logger.new(STDERR)
+LOG                   = Logger.new(STDERR)
 DEFAULT_USER_UNIQNAME = 'dueberb'
 # DEFAULT_USER_UNIQNAME = 'rsteg'
 
 #
-UNITS = GoalsViz::Unit.abbreviation_to_unit_map
-SORTED_UNITS = UNITS.values.sort { |a, b| a.abbreviation <=> b.abbreviation }
-STATUS_OPTIONS = GoalsViz::Status.order_by(:id).map { |s| [s.name, s.name] }
-STATUS_LIST = STATUS_OPTIONS.map(&:first)
-
+UNITS                 = GoalsViz::Unit.abbreviation_to_unit_map
+SORTED_UNITS          = UNITS.values.sort {|a, b| a.abbreviation <=> b.abbreviation}
+STATUS_OPTIONS        = GoalsViz::Status.order_by(:id).map {|s| [s.name, s.name]}
+STATUS_LIST           = STATUS_OPTIONS.map(&:first)
 
 
 def goal_form_locals(user, goal=nil)
@@ -36,22 +35,22 @@ def goal_form_locals(user, goal=nil)
 
   gforme = Forme::Form.new(goal)
 
-  forme = Forme::Form.new
-  units = GoalsViz::Unit.all.sort { |a, b| a.lastname <=> b.lastname }
+  forme                   = Forme::Form.new
+  units                   = GoalsViz::Unit.all.sort {|a, b| a.lastname <=> b.lastname}
   interesting_goal_owners = SORTED_UNITS + [user]
-  status_options = GoalsViz::Status.order_by(:id).map { |s| [s.name, s.name] }
+  status_options          = GoalsViz::Status.order_by(:id).map {|s| [s.name, s.name]}
 
   {
-      forme: forme,
-      user: user,
-      units: SORTED_UNITS,
-      status_options: status_options,
-      selected_status: goal.status.nil? ? status_options[0][0] : goal.status,
-      goal: goal,
-      gforme: gforme,
-      goalowners_to_show_goals_for: interesting_goal_owners,
+      forme:                             forme,
+      user:                              user,
+      units:                             SORTED_UNITS,
+      status_options:                    status_options,
+      selected_status:                   goal.status.nil? ? status_options[0][0] : goal.status,
+      goal:                              goal,
+      gforme:                            gforme,
+      goalowners_to_show_goals_for:      interesting_goal_owners,
       selectize_associated_goal_options: goal_list_for_selectize(interesting_goal_owners),
-      parent_goal_ids: goal.parent_goals.map(&:id)
+      parent_goal_ids:                   goal.parent_goals.map(&:id)
 
 
   }
@@ -64,10 +63,10 @@ def goal_list_for_selectize(list_of_owners)
 
   list_of_owners.map(&:reload).map(&:goals).flatten.uniq.map do |g|
     {
-        title: g.title,
-        uid: g.id,
-        year: g.goal_year,
-        domain: g.owner_names.join(', '),
+        title:       g.title,
+        uid:         g.id,
+        year:        g.goal_year,
+        domain:      g.owner_names.join(', '),
         description: g.description || "[no description given]"
     }
   end.to_json
@@ -100,12 +99,12 @@ class LITGoalsApp < Roda
     r.on "litgoals" do
 
       # Set up some useful data for later on
-      uniqname = r.env['HTTP_X_REMOTE_USER'] || DEFAULT_USER_UNIQNAME
-      user = GoalsViz::Person.find(uniqname: uniqname)
+      uniqname  = r.env['HTTP_X_REMOTE_USER'] || DEFAULT_USER_UNIQNAME
+      user      = GoalsViz::Person.find(uniqname: uniqname)
       currentFY = GoalsViz::FiscalYear.new
 
       common_locals= {
-          user: user,
+          user:                user,
           current_fiscal_year: currentFY
       }
 
@@ -122,19 +121,23 @@ class LITGoalsApp < Roda
       r.on 'goal' do
         r.get /(\d+)/ do |id|
           g = GoalsViz::GoalSearchResult.new(GoalsViz::Goal[id.to_i])
-          view 'goal/single_page', locals: common_locals.merge(goal: g)
+          if g.viewable_by?(user)
+            view 'goal/single_page', locals: common_locals.merge(goal: g)
+          else
+            response.status = 404
+          end
+
         end
       end
 
       r.on 'goals' do
         r.is do
-          goals = GoalsViz::Goal.all_viewable_by(user)
+          goals  = GoalsViz::Goal.all_viewable_by(user)
           locals = common_locals.merge ({
-              goals: goals.map{|g| GoalsViz::GoalSearchResult.new(g)},
-              # goal_list_for_display: display_list.to_json,
-              units: SORTED_UNITS,
+              goals:    goals.map {|g| GoalsViz::GoalSearchResult.new(g)},
+              units:    SORTED_UNITS,
               statuses: STATUS_LIST,
-              filter: Filter.new(r.params)
+              filter:   Filter.new(r.params)
           })
           view 'goals', locals: locals
         end
@@ -144,7 +147,7 @@ class LITGoalsApp < Roda
 
       r.on "create" do
         r.get do
-          locals = common_locals.merge goal_form_locals(user, flash[:bad_goal])
+          locals                           = common_locals.merge goal_form_locals(user, flash[:bad_goal])
           locals[:two_years_of_fy_options] = currentFY.select_list(2)
 
           @pagetitle = 'Create a new goal'
@@ -155,22 +158,22 @@ class LITGoalsApp < Roda
         r.post do
 
           validation = GoalsViz::GoalSchema.(r.params)
-          errors = validation.messages(full: true)
+          errors     = validation.messages(full: true)
 
 
           is_newgoal = r.params['goal_id'].nil?
 
-          g = GoalsViz::GoalForm.goal_from_form(r.params)
+          g                  = GoalsViz::GoalForm.goal_from_form(r.params)
           g.creator_uniqname ||= user.uniqname
 
           if errors.size > 0
             LOG.warn "Problem: #{errors.values}"
             flash[:error_msg] = errors.values
-            flash[:bad_goal] = g
+            flash[:bad_goal]  = g
             r.redirect
           else
             g.save
-            action = is_newgoal ? "added" : "edited"
+            action                 = is_newgoal ? "added" : "edited"
             flash[:goal_added_msg] = "Goal \"#{g.title}\" #{action}"
             sleep 0.5
             r.redirect currentFY.goals_url
@@ -186,10 +189,10 @@ class LITGoalsApp < Roda
         end
 
         r.get do
-          goal = GoalsViz::Goal.find(id: goalid.to_i)
-          locals = common_locals.merge goal_form_locals(user, goal)
+          goal                             = GoalsViz::Goal.find(id: goalid.to_i)
+          locals                           = common_locals.merge goal_form_locals(user, goal)
           locals[:two_years_of_fy_options] = currentFY.select_list(2)
-          @pagetitle = "Edit '#{goal.title}'"
+          @pagetitle                       = "Edit '#{goal.title}'"
           view "create", locals: locals
         end
       end
@@ -197,7 +200,7 @@ class LITGoalsApp < Roda
 
       r.on 'api' do
         r.on 'env' do
-          "<pre>" + r.env.keys.sort.map { |k| "#{k} => #{r.env[k]}" }.join("\n") + "</pre>"
+          "<pre>" + r.env.keys.sort.map {|k| "#{k} => #{r.env[k]}"}.join("\n") + "</pre>"
         end
 
         r.get 'user/:uniqname' do |uniqname|
