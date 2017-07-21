86c04f54d1d659fa97e6f09e95205827bc1556cb
Bill Dueber <bill@dueber.com>
2017-07-21 10:21:48 -0400

    Set up sqlite3 again

--- db/migration_20170530.sql
+++ db/migration_20170530.sql
@@ -7,3 +7,4 @@ create table goaltosteward (goalid integer unsigned, stewardid integer unsigned)
 -- also, create a fulltext index for searching
 # drop index title_desc_fulltext on goal;
 create fulltext index title_desc_fulltext on goal(title, description);
+
